690454fd59472e7e80dc88692296dda192ad637e
unknown <stefan.luthardt@outlook.com>
2022-01-06 22:54:32 +0100

    example usage of modules

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import threading
 from TS3Bot import TS3Bot
-from configuration import BOT_NAME
+
+from modules.move_afk import move_afk
+from modules.holiday_doodle import set_holiday_doodle
 
 
 def main():
     bot = TS3Bot()
-    bot.set_bot_name(BOT_NAME)
-    bot.send_channel_message('Hello channel!')
+
+    holiday_doodle_thread = threading.Thread(target=set_holiday_doodle, kwargs={'bot': bot})
+    move_afk_thread = threading.Thread(target=move_afk, kwargs={'bot': bot})
+
+    holiday_doodle_thread.start()
+    move_afk_thread.start()
+
+    move_afk_thread.join()
+
     bot.exit()
 
 
