f37f33ea01b072e0d593d82f67466794286a72b7
Stefan Luthardt <80166473+stefanluth@users.noreply.github.com>
2022-01-06 23:40:53 +0100

    update configuration.example.py

--- configuration.example.py
+++ configuration.example.py
@@ -1,8 +1,28 @@
+# Bot
 BOT_NAME = 'Bot'
 BOT_DESC = ''
 BOT_AVAT = ''
 
-AFK_CHANNELS = [13, 37, 1337]
-BUSY_CHANNEL = 37
+# Channels
+AFK_CHANNELS = [345, 567, 678, 789]
+BUSY_CHANNEL = 345  # Busy
 
-DEFAULT_HOST_BANNER_IMAGE = ''
+# Module specifics
+CHECK_AFK_INTERVAL_SECONDS = 10
+IDLE_MINUTES_BEFORE_AFK = 30
+
+CRACKERBARREL_REMINDER_3_DAYS = 'Crackerbarrel meeting in 3 days!'
+CRACKERBARREL_REMINDER_TODAY = 'Crackerbarrel meeting today 8pm!'
+
+BANNERS = {
+    'default': 'https://your-domain.com/files/banner-default.png',
+    'covid': 'https://your-domain.com/files/banner-corona.png',
+    'valentines': 'https://your-domain.com/files/banner-valentines.png',
+    'eastern': 'https://your-domain.com/files/banner-eastern.png',
+    'april fools': 'https://your-domain.com/files/banner-april-fools.png',
+    'mothers day': 'https://your-domain.com/files/banner-mothers-day.png',
+    'fathers day': 'https://your-domain.com/files/banner-fathers-day.png',
+    'halloween': 'https://your-domain.com/files/banner-halloween.png',
+    'christmas': 'https://your-domain.com/files/banner-christmas.png',
+    'new years': 'https://your-domain.com/files/banner-new-year.png',
+           }
