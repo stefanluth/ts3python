aba0206d46549a063a67f4063073559b17a6abb0
unknown <stefan.luthardt@outlook.com>
2022-01-08 23:53:36 +0100

    loop update_wordpress

--- modules/update_wordpress.py
+++ modules/update_wordpress.py
@@ -1,3 +1,5 @@
+import time
+
 from SQLiteDB import SQLiteDB
 from HTMLTable import HTMLTable
 from WordpressDB import WordpressDB
@@ -10,14 +12,16 @@ def format_time_from_seconds(seconds):
 
 
 def update_wordpress(profile_db: SQLiteDB, wordpress_db: WordpressDB, interval: int):
-    table = HTMLTable(columns=4)
-    table.add_header('Rang', 'Name', 'Verbunden', 'Davon AFK')
-    sorted_profiles = sorted(profile_db.get_all_profiles(), key=lambda p: p['connected_total'], reverse=True)
-    for rank, profile in enumerate(sorted_profiles):
-        table.add_row(rank+1,
-                      profile['nickname'],
-                      format_time_from_seconds(profile['connected_total']),
-                      format_time_from_seconds(profile['connected_afk']))
+    while 1:
+        table = HTMLTable(columns=4)
+        table.add_header('Rang', 'Name', 'Verbunden', 'Davon AFK')
+        sorted_profiles = sorted(profile_db.get_all_profiles(), key=lambda p: p['connected_total'], reverse=True)
+        for rank, profile in enumerate(sorted_profiles):
+            table.add_row(rank+1,
+                          profile['nickname'],
+                          format_time_from_seconds(profile['connected_total']),
+                          format_time_from_seconds(profile['connected_afk']))
 
-    table_html = table.generate()
-    wordpress_db.update_post_content(table_html)
+        table_html = table.generate()
+        wordpress_db.update_post_content(table_html)
+        time.sleep(interval)
