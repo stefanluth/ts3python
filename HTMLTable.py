2f6ae128e4c54c10089b87163bd9c8f02b7bc206
unknown <stefan.luthardt@outlook.com>
2022-01-08 20:52:02 +0100

    add wordpress connection to display measured client times

---
+++ HTMLTable.py
@@ -0,0 +1,33 @@
+class HTMLTable:
+    def __init__(self, columns: int):
+        self.columns = columns
+        self.header = str()
+        self.rows = list()
+
+    def add_header(self, *cells):
+        if len(cells) != self.columns:
+            return
+        self.header = '<thead><tr>'
+        for cell in cells:
+            self.header += f'<th>{cell}</th>'
+        self.header += '</tr></thead>'
+
+    def add_row(self, *cells):
+        if len(cells) != self.columns:
+            return
+        self.rows.append('<tr>')
+        for cell in cells:
+            self.rows.append(f'<td>{cell}</td>')
+        self.rows.append('</tr>')
+
+    def generate(self):
+        html = '<figure class="wp-block-table tg is-style-stripes"><table>'
+        if len(self.header) > 0:
+            html += self.header
+        if len(self.rows) > 0:
+            html += '<tbody>'
+            for row in self.rows:
+                html += row
+            html += '</tbody>'
+        html += '</figure></table>'
+        return html
---
+++ WordpressDB.py
@@ -0,0 +1,19 @@
+import mysql.connector as mysql
+
+
+class WordpressDB:
+    def __init__(self, host, database, user, password, page):
+        self.connection = mysql.connect(host=host, database=database, user=user, password=password)
+        self.cursor = self.connection.cursor(dictionary=True)
+        self.post = page
+
+    def update_post_content(self, content):
+        self.cursor.execute(fr"UPDATE `wp_posts` SET `post_content` = '{content}' WHERE `post_name` = '{self.post}'")
+        return self.connection.commit()
+
+    def get_post_content(self):
+        self.cursor.execute(fr"SELECT `post_content`  FROM `wp_posts` WHERE `post_name` = '{self.post}'")
+        return self.cursor.fetchone()
+
+
+
