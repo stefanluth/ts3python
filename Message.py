c09fba90fbefbe7be5c457f2eeab2da27ff3a6a5
unknown <stefan.luthardt@outlook.com>
2022-01-09 03:29:43 +0100

    add Message class

---
+++ Message.py
@@ -0,0 +1,19 @@
+msg_types = {
+    1: 'private',
+    2: 'channel',
+    3: 'server',
+}
+
+
+class Message:
+    def __init__(self, message: dict):
+        self.invoker_name = message['invokername']
+        self.invoker_id = message['invokerid']
+        self.invoker_unique_id = message['invokeruid']
+        self.type = msg_types[message['targetmode']]
+        self.content = self.parse_content(str(message['msg']))
+        self.raw = message
+
+    @staticmethod
+    def parse_content(message: str):
+        return message.replace(r'\s', ' ')
