ba9d949e357e121d35239952b1d597d6c81a6e86
unknown <stefan.luthardt@outlook.com>
2022-01-09 04:14:57 +0100

    rename configuration constants

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import threading
 
 from credentials import *
-from configuration import BOT_NAME, PROFILES_DB_NAME, MEASUREMENT_INTERVAL_SECONDS, WORDPRESS_INTERVAL_SECONDS
+from configuration import BOT_NAME, PROFILES_DB_NAME, MEASUREMENT_INTERVAL_SECONDS, WORDPRESS_UPDATE_INTERVAL_SECONDS
 from modules.crackerbarrel_reminder import crackerbarrel_reminder
 from modules.holiday_doodle import set_holiday_doodle
 from modules.move_afk import move_afk
@@ -39,7 +39,7 @@ def main():
                                                kwargs={
                                                    'profile_db': profile_db,
                                                    'wordpress_db': wordpress_db,
-                                                   'interval': WORDPRESS_INTERVAL_SECONDS,
+                                                   'interval': WORDPRESS_UPDATE_INTERVAL_SECONDS,
                                                })
 
     crackerbarrel_reminder_thread.start()
