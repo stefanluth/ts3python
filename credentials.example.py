e95ec66d0e3075f0e1a4b950bbd50a575eef9d4e
unknown <stefan.luthardt@outlook.com>
2022-01-09 04:19:46 +0100

    update credentials.example.py

--- credentials.example.py
+++ credentials.example.py
@@ -1,6 +1,11 @@
 SERVER_IP = ''  # your.domain or ip 133.345.337
 SERVER_PORT = 1337  # port used when clients connect to the server
-TELNET_LOGIN = ''  # telnet login name
-TELNET_PW = ''  # telnet login password
+TELNET_LOGIN = ''  # telnet_login_name
+TELNET_PW = ''  # Teln3t_p4$5w0rd1337?!
 TELNET_PORT = 13337  # telnet port
 SERVER_ID = 1337  # virtualserver_id
+
+MYSQL_HOST = ''  # mysql.domain.com (no port) or ip 133.337.333 (no port)
+MYSQL_DB_NAME = ''  # mysql_database_name
+MYSQL_USER = ''  # database_login_name
+MYSQL_PW = ''  # Dat4bas3_p4$5w0rd1234!
