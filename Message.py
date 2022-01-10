089e0aeeef2f3f1fbc6324194fa1984004dbd257
unknown <stefan.luthardt@outlook.com>
2022-01-10 03:27:12 +0100

    add Message.mark_as_used() and Message.used

--- Message.py
+++ Message.py
@@ -13,7 +13,11 @@ class Message:
         self.type = msg_types[message['targetmode']]
         self.content = self.parse_content(str(message['msg']))
         self.raw = message
+        self.used = False
 
     @staticmethod
     def parse_content(message: str):
-        return message.replace(r'\s', ' ')
+        return message.replace(r'\s', ' ').strip()
+
+    def mark_as_used(self):
+        self.used = True
