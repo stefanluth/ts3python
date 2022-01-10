7cc82f568059d94b67f2d9a7299493e796128c59
unknown <stefan.luthardt@outlook.com>
2022-01-10 02:24:53 +0100

    update configurations

--- configuration.example.py
+++ configuration.example.py
@@ -14,6 +14,11 @@ CHECK_AFK_INTERVAL_SECONDS = 10
 MEASUREMENT_INTERVAL_SECONDS = 5
 AFK_MOVED_MESSAGE = 'You were moved because of inactivity.'
 
+DO_NOT_TRACK_TOGGLE_COMMAND = '!toggle'
+DO_NOT_TRACK_CONFIRMED_MSG = f'Your connection time is not being tracked anymore. ' \
+                             f'{DO_NOT_TRACK_TOGGLE_COMMAND} to undo.'
+TRACKING_INFO_MSG = f'Your connection time is being tracked. To refuse tracking, enter {DO_NOT_TRACK_TOGGLE_COMMAND}.'
+
 CRACKERBARREL_REMINDER_3_DAYS = 'Crackerbarrel meeting in 3 days!'
 CRACKERBARREL_REMINDER_TODAY = 'Crackerbarrel meeting today 8pm!'
 
