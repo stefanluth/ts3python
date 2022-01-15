059c8d611670406cbff8f79f3d75688e9531be67
unknown <stefan.luthardt@outlook.com>
2022-01-16 00:31:16 +0100

    add fetch_one_dict to SQLiteDB

--- SQLiteDB.py
+++ SQLiteDB.py
@@ -5,7 +5,7 @@ import threading
 class SQLiteDB:
     def __init__(self, name):
         self.name = name
-        self.connection = sqlite3.connect(f'{self.name}.db', check_same_thread=False)
+        self.connection = sqlite3.connect(f'{name}.db', check_same_thread=False)
         self.connection.row_factory = sqlite3.Row
         self.cursor = self.connection.cursor()
         self.lock = threading.Lock()
@@ -26,6 +26,15 @@ class SQLiteDB:
             result = self.cursor.fetchone()
 
         if result is None:
-            return result
+            return
 
         return result[0]
+
+    def fetch_one_dict(self):
+        with self.lock:
+            result = self.cursor.fetchone()
+
+        if result is None:
+            return
+
+        return dict(result)
